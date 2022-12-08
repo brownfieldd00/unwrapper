affcd02086a45baf1963b7226f2a72f84cb39808
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-08 00:03:24 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,23 +1,40 @@
 util = {}
 util.functions = {}
-
+util.services = {}
 util.empty = function()
     return {}
 end
+util.zero = 0
 function util:get(url)
     return game:HttpGet(url)
 end
 function util:loadUiLibrary()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/brownfieldd00/ui-engine-v2/main/library.lua"))()--
 end
-function util.getVar(x)
-    return getfenv()[x] or util.empty
-end
 function util:getVariable(a)
-    return util.getVar(a)
+    return getfenv()[a] or util.empty
 end
 function util:registerFunction(name, callback)
     util.functions[name] = callback
     return true, util.functions[name]
+end
+function util:getCallback(callback)
+    return {callback()}
+end
+function util:GetServices()
+    util.services = {}
+    local _ = game:GetService('ReplicatedStorage')
+    for i, v in pairs(game:GetChildren()) do
+        local success, ret = pcall(function()
+            local _ = game:GetService(v.Name)
+            return _
+        end)
+        if success then
+            util.services[v.Name] = ret
+        end
+    end
+end
+function util:Get(x)
+    return rawget(util.services, x)
 end
 return util
