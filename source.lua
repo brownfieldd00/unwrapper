e8289d95667dce207387a2dcbbd8e3f8f2bbe13a
brownfieldd00 <brownfieldd00@gmail.com>
2022-12-08 17:14:53 -0500

    Update source.lua
    Switch util inside util:functions for self.
    + util:getModulesFromGC()

--- source.lua
+++ source.lua
@@ -12,20 +12,20 @@ function util:loadUiLibrary()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/brownfieldd00/ui-engine-v2/main/library.lua"))()--
 end
 function util:getVariable(a)
-    return getfenv()[a] or util.empty
+    return getfenv()[a] or self.empty
 end
 function util:registerFunction(name, callback)
-    util.functions[name] = callback
-    return true, util.functions[name]
+    self.functions[name] = callback
+    return true, self.functions[name]
 end
 function util:getCallback(callback)
     return {callback()}
 end
 function util:getF(a)
-    return util.functions[a]
+    return self.functions[a]
 end
 function util:GetServices()
-    util.services = {}
+    self.services = {}
     local _ = game:GetService('ReplicatedStorage')
     for i, v in pairs(game:GetChildren()) do
         local success, ret = pcall(function()
@@ -33,16 +33,16 @@ function util:GetServices()
             return _
         end)
         if success then
-            util.services[v.Name] = ret
+            self.services[v.Name] = ret
         end
     end
     return util.services
 end
 function util:Get(x)
-    return rawget(util.services, x)
+    return rawget(self.services, x)
 end
 function util:InitFunctions()
-    return util.functions
+    return self.functions
 end
 function util:executeNow(func, ...)
     local success, ret = pcall(function(...)
@@ -83,6 +83,17 @@ function util:tween(obj, prop, duration)
     local Tween = TweenService:Create(obj, TweenInfo.new(duration), prop)
     Tween:Play()
     return Tween
+end
+function util:getModulesFromGC()
+    local modules = {}
+    local gc = getgc(true)
+    for i, module in pairs(gc) do
+        if type(module) == 'table' then
+            table.insert(modules, module)
+        end
+    end
+    self.gcmodules = modules
+    return self.gcmodules
 end
 util:GetServices()
 return util
